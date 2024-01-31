95e2aff7f014516e2d93c58784c81e0c6e0e9d0b
Muarem <s.muarem10@gmail.com>
2024-01-31 22:37:19 +0400

    user permissions completed

--- app/src/main/java/com/imsoft/kotlinartbook/DetailsActivity.kt
+++ app/src/main/java/com/imsoft/kotlinartbook/DetailsActivity.kt
@@ -1,20 +1,148 @@
 package com.imsoft.kotlinartbook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.imsoft.kotlinartbook.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private var selectedBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        registerLauncher()
     }
 
     fun saveBtnClick(view: View) {}
-    fun selectImageClick(view: View) {}
+    fun selectImageClick(view: View) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+
+            if (ContextCompat.checkSelfPermission(
+                    this@DetailsActivity,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this@DetailsActivity,
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    Snackbar.make(view, "Permission Needed", Snackbar.LENGTH_INDEFINITE).setAction("OK", View.OnClickListener {
+                        // request permission
+                        permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                    }).show()
+                } else {
+                    // request permission
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+
+            } else {
+
+                val intentGalery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentGalery)
+
+            }
+
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    this@DetailsActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this@DetailsActivity,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    Snackbar.make(view, "Permission Needed", Snackbar.LENGTH_INDEFINITE).setAction("OK", View.OnClickListener {
+                        // request permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }).show()
+                } else {
+                    // request permission
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+
+            } else {
+
+                val intentGalery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentGalery)
+
+            }
+        }
+
+
+
+    }
+
+    private fun registerLauncher() {
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+
+                if (intentFromResult != null) {
+                    val imageUri = intentFromResult.data
+
+                    if (imageUri != null) {
+                        try {
+
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(this@DetailsActivity.contentResolver, imageUri)
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            } else {
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(this@DetailsActivity.contentResolver, imageUri)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+
+            if (result) {
+                // premission granted
+                val intentGalery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentGalery)
+            } else {
+                // premission denied
+                Toast.makeText(this@DetailsActivity, "Permission Needed", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
 }
