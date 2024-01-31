2d1df55bb7253653dd605682ea2514406accc1c0
Muarem <s.muarem10@gmail.com>
2024-01-31 19:41:00 +0400

    option menu created

--- app/src/main/java/com/imsoft/kotlinartbook/MainActivity.kt
+++ app/src/main/java/com/imsoft/kotlinartbook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.imsoft.kotlinartbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.imsoft.kotlinartbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,4 +16,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+
+        menuInflater.inflate(R.menu.menu_artbook, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.menuItem) {
+
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
